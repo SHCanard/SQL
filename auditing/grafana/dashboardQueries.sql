16f01a8e77de11e599b00b572e47edb4ce6ac72f
SHCanard <36749089+SHCanard@users.noreply.github.com>
2022-11-18 10:01:26 +0100

    Create dashboardQueries.sql

---
+++ auditing/grafana/dashboardQueries.sql
@@ -0,0 +1,70 @@
+---
+-- Failed logins gauge
+---
+
+SELECT COUNT(*)
+FROM [DB_Audit].[dbo].[SQLAUDIT_LOGIN]
+WHERE succeeded = 'false'
+AND server_principal_name != ''
+AND $__timeFilter([event_time])
+
+---
+-- Failed logins table
+---
+
+SELECT [event_time]
+      ,[succeeded]
+      ,[server_principal_name]
+      ,[server_instance_name]
+      ,[statement]
+      ,[client_ip]
+      ,[application_name]
+  FROM [DB_Audit].[dbo].[SQLAUDIT_LOGIN]
+  WHERE succeeded = 'false'
+  AND server_principal_name != ''
+  AND $__timeFilter([event_time])
+
+---
+-- Last successful connexions heatmap
+---
+
+SELECT
+  $__timeEpoch([event_time]),
+  [sequence_number] as value,
+  [server_principal_name] as metric
+FROM
+  [DB_Audit].[dbo].[SQLAUDIT_LOGIN]
+WHERE
+  $__timeFilter([event_time])
+  AND server_principal_name != ''
+  AND succeeded = 'true'
+ORDER BY
+  [event_time] DESC
+  
+---
+-- Create queries gauge
+---
+  
+SELECT COUNT(*)
+  FROM [DB_Audit].[dbo].[SQLAUDIT_QUERY]
+  WHERE [statement] LIKE 'CREATE%'
+  AND database_name = 'DB'
+  AND server_principal_name != 'sa'
+  AND $__timeFilter([event_time])
+
+---
+-- Create queries table
+---
+
+  SELECT event_time
+    ,server_principal_name
+    ,SCHEMA_NAME
+    ,object_name
+    ,statement
+  FROM [DB_Audit].[dbo].[SQLAUDIT_QUERY]
+  WHERE [statement] LIKE 'CREATE%'
+  AND database_name = 'DB'
+  AND server_principal_name != 'sa'
+  AND $__timeFilter([event_time])
+  
+  ---
