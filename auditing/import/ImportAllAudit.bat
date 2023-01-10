f7ed44b823578fb0a5ddec76373987a9a72707a6
SHCanard <36749089+SHCanard@users.noreply.github.com>
2023-01-10 15:29:49 +0100

    Delete ImportAllAudit.bat

--- auditing/import/ImportAllAudit.bat
+++
@@ -1,6 +0,0 @@
-SET mypath=%~dp0
-
-CALL sqlcmd -E -d DB_Audit -i "%mypath%ImportQueryAuditLogs.sql" -o "%mypath%QueryDBAuditLogs.log"
-move D:\IN\DB1\Query\*.* D:\OUT
-CALL sqlcmd -E -d DB_Audit -i "%mypath%ImportLoginAuditLogs.sql" -o "%mypath%LoginDBAuditLogs.log"
-move D:\IN\DB1\Login\*.* D:\OUT
