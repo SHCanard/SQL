b3bc72d32afdbb4e940952d170a4e3888ff1194c
SHCanard <36749089+SHCanard@users.noreply.github.com>
2023-01-10 15:28:44 +0100

    Create importQueryAudit.ps1

---
+++ auditing/setup_destination/importQueryAudit.ps1
@@ -0,0 +1,43 @@
+# Import the SQL Server PowerShell module
+#Import-Module sqlserver
+
+#Set the execution to stop after an error occurred
+$ErrorActionPreference = 'Stop'
+
+## Import Login Query logs
+
+# Connect to the SQL server
+$conn = New-Object System.Data.SqlClient.SqlConnection
+$conn.ConnectionString = "Server=localhost;Database=DB_Audit;Trusted_Connection=True;"
+$conn.Open()
+
+# Create the SQL command to execute the insert procedure
+$cmd = New-Object System.Data.SqlClient.SqlCommand
+$cmd.Connection = $conn
+$cmd.CommandText = "EXECUTE dbo.SqlAuditCaptureQueryAuditLogs"
+
+# Execute the insert procedure
+Try
+{
+    $rowsInserted=$cmd.ExecuteNonQuery()
+}
+Catch
+{
+    # Log the error message to the log file
+    Add-Content -Path "$PSScriptRoot/QueryAuditLogs_errors.log" -Value $(Get-Date)
+    Add-Content -Path "$PSScriptRoot/QueryAuditLogs_errors.log" -Value $Error[0]
+    Add-Content -Path "$PSScriptRoot/QueryAuditLogs_errors.log" -Value "---"
+    move D:\IN\DB\Query\*.* D:\OUT\KO
+}
+Finally
+{
+    # Close the connection to the SQL server
+    $conn.Close()
+}
+
+# Log a success message to the log file
+Add-Content -Path "$PSScriptRoot/QueryAuditLogs_success.log" -Value $(Get-Date)
+Add-Content -Path "$PSScriptRoot/QueryAuditLogs_success.log" -Value "Insert procedure successful, $rowsInserted rows inserted"
+Add-Content -Path "$PSScriptRoot/QueryAuditLogs_success.log" -Value "---"
+move D:\IN\DB\Query\*.* D:\OUT\OK
+
