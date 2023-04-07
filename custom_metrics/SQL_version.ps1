f8a701a86aad583a22a92674d44f92cc8e49eb14
SHCanard <36749089+SHCanard@users.noreply.github.com>
2023-04-07 13:59:54 +0200

    Create SQL_version.ps1

---
+++ custom_metrics/SQL_version.ps1
@@ -0,0 +1,32 @@
+# Get SQL version
+Write-Verbose -Message "Get SQL version..."
+$FullVersion=Invoke-Sqlcmd -Query "SELECT @@VERSION;" -QueryTimeout 3
+
+#If the previous command fails (no default instance), then look for the first instance
+if (!$FullVersion)
+{
+    $FirstInstance=(get-itemproperty 'HKLM:\SOFTWARE\Microsoft\Microsoft SQL Server').InstalledInstances[0]
+    if ($FirstInstance)
+    {
+    	$FullVersion=Invoke-Sqlcmd -ServerInstance "localhost\$FirstInstance" -Query "SELECT @@VERSION;" -QueryTimeout 3
+    }
+    $FullVersion=Invoke-Sqlcmd -ServerInstance "localhost\$FirstInstance" -Query "SELECT @@VERSION;" -QueryTimeout 3
+}
+
+# If Version is found
+if ($FullVersion)
+{
+# Do some cleanup of the output
+	$SplitedOnSpace=($FullVersion | Select-Object -ExpandProperty Column1 | Out-String).Trim().Split(" ")
+  $SplitedOnParenthesis=$SplitedOnSpace[4].Split("()")
+
+# Insert here a call to the monitoring API
+#SQL Majour version: $SplitedOnSpace[3]
+#SQL CU version: $SplitedOnParenthesis[1]
+
+}
+else
+{
+	Write-Warning -Message "No SQL version found!"
+	return 1
+}
