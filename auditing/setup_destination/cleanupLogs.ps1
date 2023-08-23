4db7bdb120ff319585ac72fcc9ba6ddba0300044
SHCanard <36749089+SHCanard@users.noreply.github.com>
2023-08-23 16:32:56 +0200

    Fix files to remove path

--- auditing/setup_destination/cleanupLogs.ps1
+++ auditing/setup_destination/cleanupLogs.ps1
@@ -11,6 +11,7 @@ $ageThreshold = (Get-Date).AddMonths(-3)
 # Get all files in the directory that are older than the age threshold
 $oldFiles = Get-ChildItem $rootDirectory -File | Where-Object { $_.LastWriteTime -lt $ageThreshold }
 
+
 Add-Content -Path "$PSScriptRoot/logs/$year-$month.cleanupLogs.log" -Value $(Get-Date)
 Add-Content -Path "$PSScriptRoot/logs/$year-$month.cleanupLogs.log" -Value $rootDirectory
 
@@ -18,10 +19,10 @@ Add-Content -Path "$PSScriptRoot/logs/$year-$month.cleanupLogs.log" -Value $root
 foreach ($oldFile in $oldFiles)
   {
       # Delete the old file
-      Remove-Item $oldFile.FullName -Force
+      Remove-Item $rootDirectory\$oldFile -Force
 
       # Log the file deletion to the log file
-      Add-Content -Path "$PSScriptRoot/logs/$year-$month.cleanupLogs.log" -Value "Deleted file $oldFile.FullName"
+      Add-Content -Path "$PSScriptRoot/logs/$year-$month.cleanupLogs.log" -Value "Deleted file $oldFile"
   }
 
 Add-Content -Path "$PSScriptRoot/logs/$year-$month.cleanupLogs.log" -Value "---"
